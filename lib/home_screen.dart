442a1d5f76dcb0d8f0c7b28f9f4664f4b55d32a3
Radhikavala60 <160853802+Radhikavala60@users.noreply.github.com>
2026-02-08 11:30:58 +0530

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,1 +1,59 @@
+import 'package:flutter/material.dart';
+import '../services/api_service.dart';
+import '../model/book.dart';
 
+class HomeScreen extends StatelessWidget {
+  final ApiService apiService = ApiService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SmartStudy AI'),
+        backgroundColor: Colors.indigo,
+      ),
+      body: FutureBuilder<List<Book>>(
+        future: apiService.fetchBooks(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return GridView.builder(
+              padding: const EdgeInsets.all(10),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          snapshot.data![index].title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          "First Published: ${snapshot.data![index].firstPublishYear ?? 'N/A'}",
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error loading data'));
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
