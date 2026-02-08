c0b220794b1370b0bb7f878e5d81632f9607ca8d
Radhikavala60 <160853802+Radhikavala60@users.noreply.github.com>
2026-02-08 11:21:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const SmartStudyAI());
+}
+
+class SmartStudyAI extends StatelessWidget {
+  const SmartStudyAI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SmartStudy AI',
+      home: HomeScreen(),
+    );
+  }
+}
 
