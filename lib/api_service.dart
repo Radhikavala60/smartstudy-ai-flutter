bae8c87103d570c8eb6f9e176a27f65836fd7385
Radhikavala60 <160853802+Radhikavala60@users.noreply.github.com>
2026-02-08 11:25:22 +0530

    Update api_service.dart

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,1 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../model/book.dart';
 
+class ApiService {
+  Future<List<Book>> fetchBooks() async {
+    final response = await http.get(
+      Uri.parse(
+        'https://openlibrary.org/subjects/computer_science.json?limit=10',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List books = data['works'];
+
+      return books.map((e) => Book.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load books');
+    }
+  }
+}
