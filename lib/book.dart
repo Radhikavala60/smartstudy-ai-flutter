a101438341c5d566f72f04f9dc8afe043d5ad84a
Radhikavala60 <160853802+Radhikavala60@users.noreply.github.com>
2026-02-08 11:23:50 +0530

    Update book.dart

--- lib/book.dart
+++ lib/book.dart
@@ -1,1 +1,13 @@
+class Book {
+  final String title;
+  final int? firstPublishYear;
 
+  Book({required this.title, this.firstPublishYear});
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      title: json['title'],
+      firstPublishYear: json['first_publish_year'],
+    );
+  }
+}
